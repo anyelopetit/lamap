dc2f3c0c99f74dcf04cf92c34c56f7b59159e535
root <root@Metroid>
2017-03-21 00:35:40 -0400

    Customize section ready

--- js/customize-main.js
+++ js/customize-main.js
@@ -1,36 +1,72 @@
 $(document).ready(function(){
 
-  //RIGHT BUTTON EFFECT
+  //LEFT BUTTON EFFECT
   $('.left-button').click(function(){
-    $('.left-button .fa-gear').addClass('fa-spin');
     $(this).css('background-color','#0095FF');
     $('.left-button b').css('color','#FFF');
+    $('.left-button .fa-gear').addClass('fa-spin');
+
+    //Give back to right button its start effect
+    $('.right-button .fa-refresh').removeClass('fa-spin');
+    $('.right-button').css('background-color','inherit');
+    $('.right-button b').css('color','#000');
+
+    //Give back to middle button its start effect
+    $('.middle-button').css('background-color','inherit');
+    $('.middle-button b').css('color','#000');
   });
-  $('.left-button').blur(function(){
+
+  //--- BLUR EFFECT FOR LEFT BUTTON
+  /*$('.left-button').blur(function(){
     $(this).css('background-color','inherit');
     $('.left-button b').css('color','#000');
-    $('.left-button .fa-gear').removeClass('fa-spin');
-  });
+    //$('.left-button .fa-gear').removeClass('fa-spin');
+  });*/
 
   //MIDDLE BUTTON EFFECT
   $('.middle-button').click(function(){
     $(this).css('background-color','#0095FF');
     $('.middle-button b').css('color','#FFF');
+
+    //Give back to right button its start effect
+    $('.right-button .fa-refresh').removeClass('fa-spin');
+    $('.right-button').css('background-color','inherit');
+    $('.right-button b').css('color','#000');
+
+    //Give back to left button its start effect
+    $('.left-button .fa-gear').removeClass('fa-spin');
+    $('.left-button').css('background-color','inherit');
+    $('.left-button b').css('color','#000');
   });
-  $('.middle-button').blur(function(){
+
+
+  //----- BLUR EVENT FOR MIDDLE BUTTON
+  /*$('.middle-button').blur(function(){
     $(this).css('background-color','inherit');
     $('.middle-button b').css('color','#000');
-  });
+  });*/
 
   //LEFT BUTTON EFFECT
   $('.right-button').click(function(){
+    //Add color and effect to selected button
     $(this).css('background-color','#0095FF');
     $('.right-button b').css('color','#FFF');
     $('.right-button .fa-refresh').addClass('fa-spin');
+
+    //Give back to left button its start effect
+    $('.left-button .fa-gear').removeClass('fa-spin');
+    $('.left-button').css('background-color','inherit');
+    $('.left-button b').css('color','#000');
+    
+    //Give back to middle button its start effect
+    $('.middle-button').css('background-color','inherit');
+    $('.middle-button b').css('color','#000');
   });
-  $('.right-button').blur(function(){
+
+  //--- BLUR EVENT FOR RIGHT BUTTON
+  /*$('.right-button').blur(function(){
     $(this).css('background-color','inherit');
     $('.right-button b').css('color','#000');
-    $('.right-button .fa-refresh').removeClass('fa-spin');
-  });
+    //$('.right-button .fa-refresh').removeClass('fa-spin');
+  });*/
 });
