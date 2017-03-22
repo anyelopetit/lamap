7b232a4f476c82efff110b24c7f8b918f980dca0
EnmChs <enmachs@gmail.com>
2017-03-21 22:06:22 -0400

    Customize section done

--- js/customize-main.js
+++ js/customize-main.js
@@ -1,72 +1,45 @@
+function takeBtn($btn1, $btn2, $btn3){
+  $btn1.css('background-color','inherit');
+  $btn2.css('color','#000');
+  if($btn3){$btn3.removeClass('fa-spin');}
+};
+function giveBtn($button1, $button2, $button3){
+  $button1.css('background-color','#0095FF');
+  $button2.css('color','#FFF');
+  if($button3){$button3.addClass('fa-spin');}
+};
 $(document).ready(function(){
-
   //LEFT BUTTON EFFECT
   $('.left-button').click(function(){
-    $(this).css('background-color','#0095FF');
-    $('.left-button b').css('color','#FFF');
-    $('.left-button .fa-gear').addClass('fa-spin');
+    giveBtn($(this),$('.left-button b'),$('.left-button .fa-gear'));
 
     //Give back to right button its start effect
-    $('.right-button .fa-refresh').removeClass('fa-spin');
-    $('.right-button').css('background-color','inherit');
-    $('.right-button b').css('color','#000');
+    takeBtn($('.right-button'),$('.right-button b'),$('.right-button .fa-refresh'));
 
     //Give back to middle button its start effect
-    $('.middle-button').css('background-color','inherit');
-    $('.middle-button b').css('color','#000');
+    takeBtn($('.middle-button'),$('.middle-button b'),null);
   });
-
-  //--- BLUR EFFECT FOR LEFT BUTTON
-  /*$('.left-button').blur(function(){
-    $(this).css('background-color','inherit');
-    $('.left-button b').css('color','#000');
-    //$('.left-button .fa-gear').removeClass('fa-spin');
-  });*/
 
   //MIDDLE BUTTON EFFECT
   $('.middle-button').click(function(){
-    $(this).css('background-color','#0095FF');
-    $('.middle-button b').css('color','#FFF');
+    giveBtn($(this),$('.middle-button b'));
 
     //Give back to right button its start effect
-    $('.right-button .fa-refresh').removeClass('fa-spin');
-    $('.right-button').css('background-color','inherit');
-    $('.right-button b').css('color','#000');
+    takeBtn($('.right-button'),$('.right-button b'),$('.right-button .fa-refresh'));
 
     //Give back to left button its start effect
-    $('.left-button .fa-gear').removeClass('fa-spin');
-    $('.left-button').css('background-color','inherit');
-    $('.left-button b').css('color','#000');
+    takeBtn($('.left-button'),$('.left-button b'),$('.left-button .fa-gear'));
   });
-
-
-  //----- BLUR EVENT FOR MIDDLE BUTTON
-  /*$('.middle-button').blur(function(){
-    $(this).css('background-color','inherit');
-    $('.middle-button b').css('color','#000');
-  });*/
 
   //LEFT BUTTON EFFECT
   $('.right-button').click(function(){
     //Add color and effect to selected button
-    $(this).css('background-color','#0095FF');
-    $('.right-button b').css('color','#FFF');
-    $('.right-button .fa-refresh').addClass('fa-spin');
+    giveBtn($(this),$('.right-button b'),$('.right-button .fa-refresh'));
 
     //Give back to left button its start effect
-    $('.left-button .fa-gear').removeClass('fa-spin');
-    $('.left-button').css('background-color','inherit');
-    $('.left-button b').css('color','#000');
-    
-    //Give back to middle button its start effect
-    $('.middle-button').css('background-color','inherit');
-    $('.middle-button b').css('color','#000');
-  });
+    takeBtn($('.left-button'),$('.left-button b'),$('.left-button .fa-gear'));
 
-  //--- BLUR EVENT FOR RIGHT BUTTON
-  /*$('.right-button').blur(function(){
-    $(this).css('background-color','inherit');
-    $('.right-button b').css('color','#000');
-    //$('.right-button .fa-refresh').removeClass('fa-spin');
-  });*/
+    //Give back to middle button its start effect
+    takeBtn($('.middle-button'),$('.middle-button b'),null);
+  });
 });
