91871654587ab4bcbf6a8c0c76f00d8390ebacfc
Anyelo <anyelopetit@gmail.com>
2017-03-22 16:29:47 -0400

    minimal customize-main.js

--- js/customize-main.js
+++ js/customize-main.js
@@ -1,45 +1,27 @@
-function takeBtn($btn1, $btn2, $btn3){
-  $btn1.css('background-color','inherit');
-  $btn2.css('color','#000');
-  if($btn3){$btn3.removeClass('fa-spin');}
+function action(btn){
+  // Set color and animation to selected button
+  btn.css({'background-color':'#0095FF','color':'#FFF'});
+  btn.find('.can-spin').addClass('fa-spin');
+
+  // Set inherit color and animation to unselected buttons
+  var cousins = btn.parent().siblings().children();
+  cousins.css({'background-color':'inherit','color':'#000'});
+  cousins.find('.can-spin').removeClass('fa-spin');
 };
-function giveBtn($button1, $button2, $button3){
-  $button1.css('background-color','#0095FF');
-  $button2.css('color','#FFF');
-  if($button3){$button3.addClass('fa-spin');}
-};
+
 $(document).ready(function(){
-  //LEFT BUTTON EFFECT
+  //LEFT BUTTON
   $('.left-button').click(function(){
-    giveBtn($(this),$('.left-button b'),$('.left-button .fa-gear'));
-
-    //Give back to right button its start effect
-    takeBtn($('.right-button'),$('.right-button b'),$('.right-button .fa-refresh'));
-
-    //Give back to middle button its start effect
-    takeBtn($('.middle-button'),$('.middle-button b'),null);
+    action($(this));
   });
 
-  //MIDDLE BUTTON EFFECT
+  //MIDDLE BUTTON
   $('.middle-button').click(function(){
-    giveBtn($(this),$('.middle-button b'));
-
-    //Give back to right button its start effect
-    takeBtn($('.right-button'),$('.right-button b'),$('.right-button .fa-refresh'));
-
-    //Give back to left button its start effect
-    takeBtn($('.left-button'),$('.left-button b'),$('.left-button .fa-gear'));
+    action($(this));
   });
 
-  //LEFT BUTTON EFFECT
+  //LEFT BUTTON
   $('.right-button').click(function(){
-    //Add color and effect to selected button
-    giveBtn($(this),$('.right-button b'),$('.right-button .fa-refresh'));
-
-    //Give back to left button its start effect
-    takeBtn($('.left-button'),$('.left-button b'),$('.left-button .fa-gear'));
-
-    //Give back to middle button its start effect
-    takeBtn($('.middle-button'),$('.middle-button b'),null);
+    action($(this));
   });
 });
