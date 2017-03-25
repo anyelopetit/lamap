414982d405eef864584ee2ac5611636ccf6754b9
Anyelo <anyelopetit@gmail.com>
2017-03-25 09:47:50 -0400

    customize.js changes

--- js/customize-main.js
+++ js/customize-main.js
@@ -8,9 +8,10 @@ function action(btn){
   cousins.css({'background-color':'inherit','color':'#000'});
   cousins.find('.can-spin').removeClass('fa-spin');
 };
-<<<<<<< HEAD
 
 $(document).ready(function(){
+  // DEFAULT ACTIVE
+  action($('.left-button'));
   //LEFT BUTTON
   $('.left-button').click(function(){
     action($(this));
@@ -25,18 +26,3 @@ $(document).ready(function(){
   $('.right-button').click(function(){
     action($(this));
   });
-=======
-
-$(document).ready(function(){
-  // DEFAULT ACTIVE
-  action($('.left-button'));
-  //LEFT BUTTON
-  $('.left-button').click(function(){ action($(this)); });
-
-  //MIDDLE BUTTON
-  $('.middle-button').click(function(){ action($(this)); });
-
-  //LEFT BUTTON
-  $('.right-button').click(function(){ action($(this)); });
->>>>>>> adf4422be4576224ca983b8d4a8a3f8ef9e0f496
-});
